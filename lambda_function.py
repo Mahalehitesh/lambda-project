e104837b572277ffe766453065deae52fa8e0a0d
Hitesh Mahale <147713450+Mahalehitesh@users.noreply.github.com>
2026-04-13 23:37:51 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,10 @@
 def lambda_handler(event, context):
+    
+    # Debug print (you can see this in CloudWatch logs)
+    print("Lambda function triggered successfully")
+
+    # Sample response
     return {
         'statusCode': 200,
-        'body': 'Updated Successfully 🎉'
+        'body': 'Hello! Lambda updated from GitHub CI/CD'
     }
